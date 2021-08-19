6665449ed59b7be59b33a4a6eea9495121a49162
Margo Urey <margo@margonline.co.uk>
2021-08-19 12:43:15 +0100

    better name for api key

--- src/_data/news.js
+++ src/_data/news.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 
 const axios = require("axios");
-const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.API_KEY}&pageSize=5`;
+const url = `https://newsapi.org/v2/top-headlines?country=us&category=business&apiKey=${process.env.NEWS_API_KEY}&pageSize=5`;
 
 module.exports = async function() {
   try {
