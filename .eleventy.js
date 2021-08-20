a21acab87275509be0b05e9e04429e6bf776998d
Margo Urey <margo@margonline.co.uk>
2021-08-20 14:59:26 +0100

    client side api converted to a serverless function using netlify functions

--- .eleventy.js
+++ .eleventy.js
@@ -1,5 +1,12 @@
 module.exports = function(config) {
 
+  config.setBrowserSyncConfig({
+    https: {
+      key: '/etc/server.key',
+      cert: '/etc/server.crt'
+    }
+  });
+
   config.addPassthroughCopy("src/js");
   return {
     dir: {
